72fe9b1787d5b592fca55e002f0ed4ab478bd6fc
Mark van Seeters <zanthrax81@gmail.com>
2016-06-09 23:26:08 +0200

    - fixed a compile error due to a missing variable - added a feature to turn off headlights with a timeout - tweaked a few settings

--- rc-crawler-lights.ino
+++ rc-crawler-lights.ino
@@ -14,27 +14,30 @@ const int brake = 11; // third brake light
 
 // Settings
 const int indicatorsInterval = 500; // milliseconds between switching indicators on and off
+const int headlightsTimeout = 30000; // milliseconds of no throttle input required to turn of headlights
 const int taillightBrightness = 40; // duty cycle for tail light brightness: 0 = 0%, 255 = 100%, brakelights are 255
-const int runningLightBrightness = 30; // duty cycle for running light brightness: 0 = 0%, 255 = 100%
+const int runningLightBrightness = 255; // duty cycle for running light brightness: 0 = 0%, 255 = 100%
 const bool flipThrottle = false; // swap forward and reverse
-const bool flipSteering = false; // swap left and right indicators
+const bool flipSteering = true; // swap left and right indicators
 
 // RC pulse length, by making the deadspots a range we filter out small fluctuations in the receiver signal.
-const int rcThrottleDead = 1470;
-const int throttleDeadTreshold = 100;
+const int rcThrottleDead = 1470; // use the debugging code at the bottom to find the pulse length for center position
+const int throttleDeadTreshold = 50;
 const int throttleDeadLow = rcThrottleDead - throttleDeadTreshold;
 const int throttleDeadHigh = rcThrottleDead + throttleDeadTreshold;
 
-const int rcSteeringDead = 1530;
-const int steeringDeadTreshold = 100;
+const int rcSteeringDead = 1430;
+const int steeringDeadTreshold = 150;
 const int steeringDeadLow = rcSteeringDead - steeringDeadTreshold;
 const int steeringDeadHigh = rcSteeringDead + steeringDeadTreshold;
 
 // Store which indicators should be blinking, used to blink without using delay()
-bool indicatorSwitch[sizeof(indicators)] = {false};
-bool indicatorState[sizeof(indicators)] = {false};
-unsigned long previousIndicatorMillis[sizeof(indicators)] = {0};
+bool indicatorSwitch[sizeof(frontIndicators)] = {false};
+bool indicatorState[sizeof(frontIndicators)] = {false};
+unsigned long previousIndicatorMillis[sizeof(frontIndicators)] = {0};
 
+// Tracks how long throttle has been centered.
+unsigned long previousHeadlightsMillis = 0;
 
 // Vars for throttle and steering positions
 int throttlePosition;
@@ -95,7 +98,7 @@ void loop() {
     // no throttle signal, receiver not connected or not switched on? do nothing.
     throttleForward = false;
     throttleReverse = false;
-    throttleDead = false;
+    throttleDead = true;
   }
 
 
@@ -132,11 +135,16 @@ void loop() {
 
   /*
      Headlights.
-     Off by default, get turned on the first time driving forward.
-     @TODO: turn off headlights after a certain amount of time has passed without any thottle input.
+     Off by default, get turned when driving forward.
+     Headlights will turn off after a certain amount of time has passed without any thottle input.
   */
   if (throttleForward) {
+    previousHeadlightsMillis = currentMillis;
     digitalWrite(headlights, HIGH);
+  } else {
+    if (currentMillis - previousHeadlightsMillis >= headlightsTimeout) {
+      digitalWrite(headlights, LOW);
+    }
   }
 
   /*
@@ -159,7 +167,7 @@ void loop() {
       // Blink indicators
       if (currentMillis - previousIndicatorMillis[i] >= indicatorsInterval) {
         // Interval passed, invert indicators
-        indicatorState[i] = ! indicatorState[i];
+        indicatorState[i] = !indicatorState[i];
         previousIndicatorMillis[i] = currentMillis;
       }
     } else {
@@ -189,6 +197,9 @@ void loop() {
     Serial.print(steeringDead ? "1" : "0");
     Serial.print(steeringRight ? "1" : "0");
 
+    Serial.print(" Cmili "); // currentMillis
+    Serial.print(currentMillis);
+
     // Indicator debugging
     Serial.print(" Lmili "); // Left last timeout
     Serial.print(previousIndicatorMillis[0]);
@@ -196,6 +207,12 @@ void loop() {
     Serial.print(indicatorSwitch[0]);
     Serial.print(" Lst "); // Left LED State
     Serial.print(indicatorState[0]);
+
+    // Headlights debugging
+    Serial.print(" H ");
+    Serial.print(previousHeadlightsMillis); 
+    Serial.print(" ");
+    Serial.print(currentMillis - previousHeadlightsMillis);       
 
     // New line
     Serial.println("");
